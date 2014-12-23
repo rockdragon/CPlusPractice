94639db6a5f32045080882e49f87f03674a7e2ce
Locke <moyerock@gmail.com>
2014-12-23 21:42:28 +0800

    initial commit.

---
+++ Vector.h
@@ -0,0 +1,14 @@
+class Vector {
+private:
+    double* elem; // elem points to an array of sz doubles
+    int sz;
+public:
+    Vector(int s) :elem{new double[s]}, sz{s} // constructor: acquire resources
+    {
+        for (int i=0; i!=s; ++i) // initialize elements
+            elem[i]=0;
+    }
+    ~Vector() { delete[] elem; } // destructor: release resources
+    double& operator[](int i);
+    int size() const;
+};
